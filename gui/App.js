fe002d2c1327735e0769136e1411d9b96e17db34
dangolbeeker <scottb305@outlook.com>
2021-10-09 21:49:09 -0500

    Created Basic Drawer Nav with Nested Tab Navigation Non tested options Labels

--- gui/App.js
+++ gui/App.js
@@ -1,112 +1,53 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import 'react-native-gesture-handler';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+
+const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
+
+
+
+
+export default function App() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+
+    <Drawer.Navigator initialRouteName="CreateWallet">
+    <Drawer.Screen
+      name="Create Wallet"
+      component={CreateWallet}
+      options={{ drawerLabel: 'CreateWallet' }}
+    />
+    <Drawer.Screen
+      name="Send"
+      component={Notifications}
+      options={{ drawerLabel: 'Send' }}
+    />
+    <Drawer.Screen
+      name="Receive"
+      component={Profile}
+      options={{ drawerLabel: 'Receive' }}
+    />
+    <Drawer.Screen
+      name="Addresses"
+      component={Addresses}
+      options={{ drawerLabel: 'Addresses' }}
+    />
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen}
+        options={{ tabBarLabel: 'Home'}} />
+        <Tab.Screen name="ExportSeed" component={ExportSeedScreen}
+        options={{ tabBarLabel: 'ExportSeed'}} />
+        <Tab.Screen name="Settings" component={SettingsScreen}
+        options={{ tabBarLabel: 'Settings'}} />
+      </Tab.Navigator>
+    </NavigationContainer>
+    </Drawer.Navigator>
   );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+}
